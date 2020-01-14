acd9f17205943c543854746a38ab2b38584d2ea6
Paul Opmann <paul.opmann@gmail.com>
2020-01-14 16:30:21 +0200

    coverage script added

--- src/organisms/AdminRouter/AdminRouter.test.jsx
+++ src/organisms/AdminRouter/AdminRouter.test.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AdminRouter from './AdminRouter.jsx';
+import { BrowserRouter as Router } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<AdminRouter />, div);
+    ReactDOM.render(
+        <Router>
+            <AdminRouter />
+        </Router>,
+        div,
+    );
     ReactDOM.unmountComponentAtNode(div);
 });
 
 it('snapshot renders', () => {
-    const component = renderer.create(<AdminRouter />);
+    const component = renderer.create(
+        <Router>
+            <AdminRouter />
+        </Router>,
+    );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
